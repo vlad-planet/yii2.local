ec82930d6103ab72a579237e57ba897a0e75ca8f
vladplanet <vlad-planet@mail.ru>
2019-02-23 07:32:52 +0300

    Initial commit

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -37,6 +37,7 @@ AppAsset::register($this);
     ]);
     $menuItems = [
         ['label' => 'Home', 'url' => ['/site/index']],
+		['label' => 'Blog', 'url' => ['/blog']],
         ['label' => 'About', 'url' => ['/site/about']],
         ['label' => 'Contact', 'url' => ['/site/contact']],
     ];
